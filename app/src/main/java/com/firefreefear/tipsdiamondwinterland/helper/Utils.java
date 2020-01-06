f43b081c943e812694154108c38362759e9cc6b3
kakzi <kakzi.sholarship@gmail.com>
2020-01-06 08:42:43 +0700

    Fix Problem ya

--- app/src/main/java/com/firefreefear/tipsdiamondwinterland/helper/Utils.java
+++ app/src/main/java/com/firefreefear/tipsdiamondwinterland/helper/Utils.java
@@ -3,15 +3,14 @@ package com.firefreefear.tipsdiamondwinterland.helper;
 
 public class Utils {
 
-    public final static String BASE_URL = "https://script.google.com/macros/s/AKfycbw3KK3IxPtp4zDGAD1KUGuVRWczFy-64BYWmitdTob706v1q94/exec";
-    public final static String GET_MENU = "?action=getMenu";
-    public final static String GET_TIPS = "?action=getTips";
-    public final static String GET_WAEPON = "?action=getWaepon";
-    public final static String GET_CHARACTER = "?action=getCharacter";
-    public final static String GET_VEHICLES = "?action=getVehicles";
+    public final static String BASE_URL = "https://gist.githubusercontent.com/kakzi/";
+    public final static String GET_MENU = "9130bd561e99db51a41b2026a58d1510/raw/49e80e1775d65f0e84604e24e2d86cf5591295f8/menu.json";
+    public final static String GET_TIPS = "02f3316ff96eb64bf59828160d54496b/raw/a0c1ab7915a1ade3f760af4d777b372bcf99cbdf/tips.json";
+    public final static String GET_WAEPON = "09be88e9a32722c169e3471f139203eb/raw/a03fc804527a0a3387cc0ff45cf0ab96c355af43/waepon.json";
+    public final static String GET_CHARACTER = "ee2a18ee2878f3fbbfdd1c2bd11f4bd6/raw/5cc6e83e179935e895ffcbad23c4ef4b10f635ed/character.json";
+    public final static String GET_VEHICLES = "35b84ab436bb2db9c4cf0bd5427bc01d/raw/2a8255898faef1d88904d405185a78f0be34309e/vehicle.json";
     public final static String GET_VIDEO = "?action=getVideo";
-    public final static String GET_DIAMOND = "?action=getDiamond";
-    public final static String GET_WALLPAPER = "?action=getWallpaper";
-
+    public final static String GET_DIAMOND = "c37798b83bfa7750eca576925796c685/raw/f7e679478bc85c91ca443b53373007498076040d/diamond.json";
+    public final static String GET_WALLPAPER = "238f0f1f772d92dbe63c2904f70a68ce/raw/0884c61787e7c95b41c5fb43ed6eab3266789935/wallpaper.json";
     
 }
